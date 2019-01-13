3a83d950494b7ce73f32289a2ff312c59795c0b0
Bacs <bacs@ua.fm>
2019-01-13 21:56:53 +0200

    FTP has been connected

--- src/main/java/fm/ua/bacs/testtaskrestservice/controller/OutCheckerController.java
+++ src/main/java/fm/ua/bacs/testtaskrestservice/controller/OutCheckerController.java
@@ -1,15 +1,11 @@
 package fm.ua.bacs.testtaskrestservice.controller;
 
 import fm.ua.bacs.testtaskrestservice.helpers.FTP;
-import fm.ua.bacs.testtaskrestservice.helpers.Helper;
 import fm.ua.bacs.testtaskrestservice.helpers.Props;
 import org.apache.commons.net.ftp.FTPFile;
 
-import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -47,23 +43,6 @@ public class OutCheckerController {
     }
 
     private void checker() throws IOException {
-
-/*-----------------------        Code for working with local folders ----------------------------------*/
-
-//        Collection<File> all = new ArrayList<>();
-//        Helper helper = new Helper();
-//        helper.addTree(new File("C:\\Users\\Bacs\\Downloads\\rest-service-master\\out"), all);
-//        for (File file : all) {
-//            try {
-//                helper.moveFile(file, new File("C:\\Users\\Bacs\\Downloads\\rest-service-master\\ready\\" + file.getName()));
-//                fm.ua.bacs.testtaskrestservice.helpers.Response response = new fm.ua.bacs.testtaskrestservice.helpers.Response();
-//                System.out.println(response.makeResponse(file.getName(), "File is ready", 200));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-/*-----------------------        Code for working with local folders ----------------------------------*/
 
         FTP ftp = new FTP();
         Props props = new Props();
